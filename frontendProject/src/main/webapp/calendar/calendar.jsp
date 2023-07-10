825bc50b3f130aefd782143e9f0ee1c49927bc00
504-t <itdanja@kakao.com>
2023-07-10 11:25:55 +0900

    day24 캘린더js

--- frontendProject/src/main/webapp/calendar/calendar.jsp
+++ frontendProject/src/main/webapp/calendar/calendar.jsp
@@ -8,32 +8,26 @@
 	<link href="../css/calendar.css" rel="stylesheet">
 </head>
 <body>
+
 	<%@include file="../header.jsp" %>
-	
 	<div class="calwrap"> <!-- 캘린더 전체구역 -->
 	
 		<div class="caltop"> <!-- 상단 메뉴  -->
-			<button type="button"> < </button>  <!-- 이전 달 버튼 -->
-			<h3> 2023년 07월 </h3>				<!-- 현재 연도/월 표시 구역 -->
-			<button type="button"> > </button>  <!-- 다음 달 버튼 -->
+			<button onclick="onNext( 0 )" type="button"> < </button>  <!-- 이전 달 버튼 -->
+			<h3 class="caldate"> 2023년 07월 </h3>				<!-- 현재 연도/월 표시 구역 -->
+			<button onclick="onNext( 1 )"type="button"> > </button>  <!-- 다음 달 버튼 -->
 		</div>
 		
 		<div class="calendar"> <!-- 날짜 표시 구역  -->
 			<!-- 상단 요일 표기 일~토 -->
-			<div class="week sunday"> 일 </div> 	<div class="week"> 월 </div>
-			<div class="week"> 화 </div> 	<div class="week"> 수 </div> 
-			<div class="week"> 목 </div>		<div class="week"> 금 </div>	<div class="week" > 토 </div>
+			
 			<!-- 일 표기 1 ~ 31/30/29 -->
-			<div> 1 </div>	<div> 2 </div>	<div> 3 </div>
-			<div> 4 </div>	<div> 5 </div>	<div> 6 </div>
-			<div> 7 </div>	<div> 8 </div>	<div> 9 </div>
+			
 		</div>
 		
-	</div>
-	
-	
-	
+	</div> <!--  ----------------------------------  -->
 	<%@include file="../footer.jsp" %>
+	<script src="../js/calendar.js" type="text/javascript"></script>
 
 </body>
 </html>
---
+++ frontendProject/src/main/webapp/js/calendar.js
@@ -0,0 +1,71 @@
+console.log('calendar.js')
+// 0. 현재 연도/월 [ 초기값 ]
+	// new Date() 				: 현재 시스템 날짜/시간 반환해주는 클래스
+	// new Date( 연도 , 월 , 일 ) : 사용자 정의 날짜 반환해주는 클래스 
+		// .getFullYear() 	: 연도
+		// .getMonth() 		: 월[ 0~11 ]
+		// .getDay()		: 요일[ 일0 ~ 토6 ]
+		// .getDate()		: 일 
+		// new Date( year , month , 0 );	: [ 다음달 1일에서 -1 ] 현재 월의 마지막 일수 구하기 
+		
+let year = new Date().getFullYear();	// 현재 연도 
+let month = new Date().getMonth()+1;	// 현재 월[ 0~11 ] +1
+console.log( year ); console.log( month );
+
+// 1. 현재 연도/월 기준으로 달력 출력 하는 함수 
+calPrint();
+function calPrint(){ 
+	// 1. 현재 연도와 월 을 해당 구역에 출력하기 
+	document.querySelector('.caldate').innerHTML = `${ year }년 ${ month }월`;
+	// 1. 요일 과 일 출력 
+	
+		/* 1. 시작 요일 */	// 현재 보고 있는 캘린더의 날짜 [ ]
+		let now = new Date( year , month-1 , 1 ); 	// month-1 => 7-1 => 6 => 7월
+		let sWeek = now.getDay();	console.log( sWeek );
+		/* 2. 현재 월의 마지막 일 구하기 */
+		let now2 = new Date( year , month , 0 );	// month => 7 => 7 => 8월
+		let eDay = now2.getDate();	console.log( eDay );
+
+	let calendar = document.querySelector('.calendar');
+	let html = '';
+		// 1. 요일
+		html += `	<div class="week sunday"> 일 </div> 	<div class="week"> 월 </div>
+					<div class="week"> 화 </div> 		<div class="week"> 수 </div> 
+					<div class="week"> 목 </div>			<div class="week"> 금 </div>	
+					<div class="week"> 토 </div>`
+		// **** 현재 달력 1일의 요일까지 공백 출력 
+		for( let b = 1 ; b<=sWeek ; b++ ){ // 1부터 1일의요일까지 공백 구역 출력 
+			html += `<div></div>`
+		} // f end 
+		// 2. **** 현재 달력 마지막 일 까지 일수 출력  
+		for( let day = 1 ; day<=eDay ; day++ ){
+			html += `<div> ${ day } </div>`
+		} // f end
+	calendar.innerHTML = html;
+} // f end 
+// 2. 버튼을 클릭했을때 현재 월 변화해주는 함수 [ 인수 : 이전달(0)vs다음달(1)]
+function onNext( check ){ console.log( check );
+	// 1. 버튼 식별후 월 증감
+	if( check == 0 ){ // 이전 달 
+		month--;	// 현재 보고 있는 월 1 차감
+		// 만약에 월이 차감했는데 1보다 작아지면 12월 변경하되 연도 1차감 // 2023년도 1월 --> 1차감 --> 2022년도 12월 
+		if( month < 1 ){ month=12; year--; }
+	}else if( check == 1 ){ // 다음 달 
+		month++;	// 현재 보고 있는 월 1 증가
+		if( month > 12 ){ month = 1 ; year++; }
+	}
+	// 2. 월 증감후 새로고침 ** 
+	calPrint();
+}
+
+
+
+
+
+
+
+
+
+
+
+
