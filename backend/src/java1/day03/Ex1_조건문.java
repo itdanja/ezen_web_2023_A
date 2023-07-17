18ebd2f1d5ccf2ec806b4ac330345c8969da06e0
504-t <itdanja@kakao.com>
2023-07-17 15:11:53 +0900

    day28 조건문

---
+++ backend/src/java1/day03/Ex1_조건문.java
@@ -0,0 +1,141 @@
+package java1.day03;
+
+import java.util.Random;
+
+public class Ex1_조건문 { // class s 
+
+	public static void main(String[] args) { // main s 
+		
+		// [ p.111]
+		int score = 93;
+		// 1. 
+		if( score >= 90 ) 
+			System.out.println("1합격");
+		
+		// 2. 
+		if( score >= 90 ) 
+			System.out.println("2점수가 90보다큽니다.");
+			System.out.println("2등급은 A등급입니다.");
+		// 2. 
+		if( score < 90 ) 
+			System.out.println("2점수가 90보다 작습니다.");
+			System.out.println("2등급은 B등급입니다.");
+			
+		// 3. [ p.113]
+		if( score >= 90 ) {
+			System.out.println("3점수가 90보다큽니다.");
+			System.out.println("3등급은 A등급입니다.");
+		}else {
+			System.out.println("3점수가 90보다 작습니다.");
+			System.out.println("3등급은 B등급입니다.");
+		}
+		
+		// 4. [ p.114 ]
+		if( score >= 90 ) {
+			System.out.println("4점수가 100~90입니다.");
+			System.out.println("4등급은 A등급 입니다. ");
+		}else if( score >= 80 ) {
+			System.out.println("4점수가 80~89입니다.");
+			System.out.println("4등급은 B등급 입니다. ");
+		}else if( score >= 70 ) {
+			System.out.println("4점수가 70~79입니다.");
+			System.out.println("4등급은 C등급 입니다. ");
+		}else {
+			System.out.println("4점수가 70 미만입니다.");
+			System.out.println("4등급은 D등급 입니다. ");
+		}
+		// ------------------------------ 
+		if( score >= 90 ) {
+			System.out.println("5점수가 100~90입니다.");
+			System.out.println("5등급은 A등급 입니다. ");
+		}if( score >= 80 ) {
+			System.out.println("5점수가 80~89입니다.");
+			System.out.println("5등급은 B등급 입니다. ");
+		}if( score >= 70 ) {
+			System.out.println("5점수가 70~79입니다.");
+			System.out.println("5등급은 C등급 입니다. ");
+		}if( score < 70){
+			System.out.println("5점수가 70 미만입니다.");
+			System.out.println("5등급은 D등급 입니다. ");
+		}
+		
+		// [ p.115 ] 난수생성 :  Math.random()
+		System.out.println( Math.random() ); // 0.0<= ~ <1.0
+		System.out.println( Math.random()*6); // 0.0 <= ~ < 6.0
+		System.out.println( (int)(Math.random()*6) ); // 0 <= ~ <6
+		System.out.println( (int)(Math.random()*6) +1 ); // 1 <= ~ <7
+		// 정수 난수공식 :  (int) (Math.random() * n ) + s 
+			// s : 난수 시작번호		n : 난수 개수 
+		// 예시) 1~45
+		System.out.println( (int)(Math.random()*45)+1 );
+		// -------------------------- vs -------------------------------
+		// 난수 생성 클래스 : Random 클래스 
+			// 1. Random 객체 생성한다. 
+				// Random random = new Random();
+			// 2. 랜덤 값 호출
+				// random.next타입명() : 타입의 허용범위내 난수 생성  
+		Random random = new Random();
+		System.out.println( random.nextInt() ) ;		// -21억 ~ +21억
+		System.out.println( random.nextInt(3) ) ; 		// 0~2
+		// random.nextInt( 개수 ) + 시작번호
+		System.out.println( random.nextInt(6) +1  ) ; 	// 1~6
+		System.out.println( random.nextInt(45)+1 ); 	// 1~45
+		// 예시) a-z
+		System.out.println( (char)(random.nextInt(26) + 97) );	// 97~122
+		
+		// [ p.116 ] 주사위 예제
+		int num = (int)( Math.random()*6 ) +1;  // 1~6
+			int num2 = random.nextInt(6)+1;			// 1~6
+		
+		if( num == 1 ) { System.out.println("1나왔습니다.");}
+		else if( num == 2 ) { System.out.println("2나왔습니다.");}
+		else if( num == 3 ) { System.out.println("3나왔습니다.");}
+		else if( num == 4 ) { System.out.println("4나왔습니다.");}
+		else if( num == 5 ) { System.out.println("5나왔습니다.");}
+		else { System.out.println("6나왔습니다");}
+		
+	} // main e 
+	
+} // class e
+
+/*
+ 	if : 조건에 따른 흐름 제어 
+ 		형태
+ 			1. if( 조건식 ) 실행문;
+ 			
+ 			2. if( 조건식 ) { 실행문; 실행문; }
+ 				* 만약에 참/거짓의 실행문이 2개 이상일경우 { } 묶음
+ 				
+			3. if( 조건식 ) { 참일경우실행문 }
+			   else{ 거짓일경우실행문 }
+			   
+		   	4. if( 조건식1 ) { 참1 일 경우 실행문 }
+		   		else if( 조건식2 ) { 참2 일 경우 실행문 }
+		   		else if( 조건식3 ) { 참3 일 경우 실행문 }
+		   		else{ 거짓 일 경우 실행문 }
+		   			* [ 다수 조건에 하나의 실행문 처리 ]
+   			4
+   				if( 조건식1 ) { 참1 일경우 실행문 }
+   				if( 조건식2 ) { 참2 일경우 실행문 }
+   				if( 조건식3 ) { 참3 일경우 실행문 }
+   				if( 조건식4 ) { 참4 일경우 실행문 }
+			   		* [ 다수 조건에 다수 실행문 처리 ]
+			   		
+	   		5. if중첩 
+	   			if( ) {
+	   				if( ) { }
+	   			}else{
+	   				if( ) { }
+	   			}
+ 
+ */
+
+
+
+
+
+
+
+
+
+
