78131ba090c6515a1ea28041379972b9380e51e3
504-t <itdanja@kakao.com>
2023-07-19 10:37:15 +0900

    day30 과제4-1 풀이

--- backend/src/과제/과제4/과제4_1_예출금_1.java
+++ backend/src/과제/과제4/과제4_1_예출금_1.java
@@ -2,26 +2,29 @@ package 과제.과제4;
 
 import java.util.Scanner;
 
-public class 과제4_1_예출금 { 
+public class 과제4_1_예출금_1 { 
 	public static void main(String[] args) { 
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		int balance = 0; String account = "123-1234"; short password = 1234;
+		int balance = 0; String account = "123-1234"; short password = 1234; // 1명의 임의 계좌 
 		
 		while( true ) { // 무한루프 [ 종료조건 : 4 입력시 break; ] 
 			System.out.println("\n\n--------------------");
 			System.out.println("1.예금 2.출금 3.잔액 4.종료 : ");
 			System.out.println("--------------------");
 			System.out.print(">> 선택 : ");
-			int ch = scanner.nextInt();
+			int ch = scanner.nextInt(); // 입력받기 
 			
 			/* 문제풀이 위치 */
 			
+			// step1 : 입력받은 메뉴 번호에 따른 서로 다른 코드/행동 실행
+			if( ch == 1  ) {}
+			else if( ch == 2  ) {}
+			else if( ch == 3  ) {}
+			else if( ch == 4  ) {}
 			
 			/* ----------- */
-			
-		
 		} 
 	} 
 } 
@@ -36,7 +39,7 @@ public class 과제4_1_예출금 {
 			1. 사용할 기능을 입력받는다. [1 이면 예금기능 2 이면 출금기능 3이면 잔액확인기능 4이면 종료 ]
 			
 		실행 조건 
-			1. 만약에 예금 기능 
+			1. 만약에 예금(판단??? ch == 1) 기능 
 				1. 계좌번호를 입력받아 일치하면 예금액을 입력받아 예금액 변수에 누적 더 해준후 [ 예금 성공 ]출력 
 								일치하지 않으면 '계좌번호 미일치' 라는 안내출력후 다시 메뉴로 이동 
 			2. 만약에 출금 기능 
