8ba94b3effc11ed88f1d08aa278cecb74e117b1c
504-t <itdanja@kakao.com>
2023-07-25 12:10:13 +0900

    day35 회원제시스템

---
+++ backend/src/과제/과제8/Member.java
@@ -0,0 +1,16 @@
+package 과제.과제8;
+
+// 목적 : 설계도, 객체의모델
+public class Member {
+	
+	// 1. 필드 : 객체의 데이터 를 저장하는곳
+	String id;	
+	String password;
+	String name;
+	String phone;
+	int age;
+	
+	// 2. 생성자
+	
+	// 3. 메소드 
+}
---
+++ backend/src/복습/part2/Part2.java
@@ -0,0 +1,10 @@
+package 복습.part2;
+
+public class Part2 {
+
+	public static void main(String[] args) {
+		
+		String 재고관리 = "10,0,300\n3,0,200\n2,0,100\n";
+		
+	}
+}
